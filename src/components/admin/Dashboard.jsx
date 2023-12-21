ab462d8cec6792f595ec048d491a015810c53e69
angeldvilla <escarlata.2003@gmail.com>
2023-12-21 03:35:36 -0500

    Feature new changes

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,120 +1,120 @@
-import React, { Fragment, useEffect } from 'react'
-import { Link } from 'react-router-dom';
-import Loader from '../layout/Loader'
-import Sidebar from './Sidebar'
-import { useDispatch, useSelector } from 'react-redux'
-import { getAdminProducts } from '../../actions/productActions'
-import { allOrders } from '../../actions/orderActions'
-import { allUsers } from '../../actions/userActions'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Loader from "../layout/Loader";
+import Sidebar from "./Sidebar";
+import { useDispatch, useSelector } from "react-redux";
+import { getAdminProducts } from "../../actions/productActions";
+import { allOrders } from "../../actions/orderActions";
+import { allUsers } from "../../actions/userActions";
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-	const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products);
+  const { users } = useSelector((state) => state.allUsers);
+  const { orders, totalAmount, loading } = useSelector(
+    (state) => state.allOrders
+  );
 
-	const { products } = useSelector(state => state.products)
-	const { users } = useSelector(state => state.allUsers)
-	const { orders, totalAmount, loading } = useSelector(state => state.allOrders)
+  let outOfStock = 0;
+  products?.products?.products?.forEach((product) => {
+    if (product?.stock === 0) {
+      outOfStock += 1;
+    }
+  });
 
-	let outOfStock = 0;
-	products.forEach(product => {
-		if (product.stock === 0) {
-			outOfStock += 1;
-		}
-	})
+  useEffect(() => {
+    dispatch(getAdminProducts());
+    dispatch(allOrders());
+    dispatch(allUsers());
+  }, [dispatch]);
 
-	useEffect(() => {
-		dispatch(getAdminProducts())
-		dispatch(allOrders())
-		dispatch(allUsers())
-	}, [dispatch])
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
 
-	return (
-		<Fragment>
-			<div className="row mt-5">
-				<div className="col-12 col-md-2 mt-4">
-					<Sidebar />
-				</div>
+  return (
+    <div className="flex ">
+      <Sidebar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
 
-				<div className="col-12 col-md-10 mt-5">
-					<h1 className="my-4">Dashboard</h1>
+      <div className={`flex-1 ml-64 ${sidebarOpen ? 'ml-64' : 'ml-16'}`}>
+        <h1 className="text-3xl font-bold my-4">Admin Dashboard</h1>
 
-					{loading ? <Loader /> : (
-						<Fragment>
-							<h1>Admin Dashboard</h1>
+        {loading ? (
+          <Loader />
+        ) : (
+          <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+              <div className="card bg-primary text-white h-40">
+                <div className="card-body">
+                  <p className="text-center text-xl font-bold">
+                    Total Amount
+                    <br />
+                    <span className="text-2xl">
+                      ${totalAmount && totalAmount.toFixed(2)}
+                    </span>
+                  </p>
+                </div>
+              </div>
 
-							<div className="row pr-4">
-								<div className="col-xl-12 col-sm-12 mb-3">
-									<div className="card text-white bg-primary o-hidden h-100">
-										<div className="card-body">
-											<div className="text-center card-font-size">Total Amount<br /> <b>${totalAmount && totalAmount.toFixed(2)}</b>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
+              <Link to="/admin/products" className="card bg-success text-white h-40">
+                <div className="card-body">
+                  <p className="text-center text-xl font-bold">
+                    Products
+                    <br />
+                    <span className="text-2xl">{products && products.length}</span>
+                  </p>
+                </div>
+                <div className="card-footer text-white flex justify-between">
+                  <span>View Details</span>
+                  <i className="fa fa-angle-right"></i>
+                </div>
+              </Link>
 
-							<div className="row pr-4">
-								<div className="col-xl-3 col-sm-6 mb-3">
-									<div className="card text-white bg-success o-hidden h-100">
-										<div className="card-body">
-											<div className="text-center card-font-size">Products<br /> <b>{products && products.length}</b></div>
-										</div>
-										<Link className="card-footer text-white clearfix small z-1" to="/admin/products">
-											<span className="float-left">View Details</span>
-											<span className="float-right">
-												<i className="fa fa-angle-right"></i>
-											</span>
-										</Link>
-									</div>
-								</div>
+              <Link to="/admin/orders" className="card bg-danger text-white h-40">
+                <div className="card-body">
+                  <p className="text-center text-xl font-bold">
+                    Orders
+                    <br />
+                    <span className="text-2xl">{orders && orders.length}</span>
+                  </p>
+                </div>
+                <div className="card-footer text-white flex justify-between">
+                  <span>View Details</span>
+                  <i className="fa fa-angle-right"></i>
+                </div>
+              </Link>
 
+              <Link to="/admin/users" className="card bg-info text-white h-40">
+                <div className="card-body">
+                  <p className="text-center text-xl font-bold">
+                    Users
+                    <br />
+                    <span className="text-2xl">{users && users.length}</span>
+                  </p>
+                </div>
+                <div className="card-footer text-white flex justify-between">
+                  <span>View Details</span>
+                  <i className="fa fa-angle-right"></i>
+                </div>
+              </Link>
 
-								<div className="col-xl-3 col-sm-6 mb-3">
-									<div className="card text-white bg-danger o-hidden h-100">
-										<div className="card-body">
-											<div className="text-center card-font-size">Orders<br /> <b>{orders && orders.length}</b></div>
-										</div>
-										<Link className="card-footer text-white clearfix small z-1" to="/admin/orders">
-											<span className="float-left">View Details</span>
-											<span className="float-right">
-												<i className="fa fa-angle-right"></i>
-											</span>
-										</Link>
-									</div>
-								</div>
+              <div className="card bg-warning text-white h-40">
+                <div className="card-body">
+                  <p className="text-center text-xl font-bold">
+                    Out of Stock
+                    <br />
+                    <span className="text-2xl">{outOfStock}</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-
-								<div className="col-xl-3 col-sm-6 mb-3">
-									<div className="card text-white bg-info o-hidden h-100">
-										<div className="card-body">
-											<div className="text-center card-font-size">Users<br /> <b>{users && users.length}</b></div>
-										</div>
-										<Link className="card-footer text-white clearfix small z-1" to="/admin/users">
-											<span className="float-left">View Details</span>
-											<span className="float-right">
-												<i className="fa fa-angle-right"></i>
-											</span>
-										</Link>
-									</div>
-								</div>
-
-
-								<div className="col-xl-3 col-sm-6 mb-3">
-									<div className="card text-white bg-warning o-hidden h-100">
-										<div className="card-body">
-											<div className="text-center card-font-size">Out of Stock<br /> <b>{outOfStock}</b></div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</Fragment>
-					)}
-
-				</div>
-			</div>
-
-		</Fragment >
-	)
-}
-
-export default Dashboard
+export default Dashboard;
--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -1,85 +1,89 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaTachometerAlt, FaProductHunt, FaClipboard, FaPlus, FaShoppingBasket, FaUsers, FaStar, FaBars } from "react-icons/fa";
 
-const Sidebar = () => {
-	return (
-		<div className="sidebar-wrapper">
-			<nav id="sidebar">
-				<ul className="list-unstyled components mt-4">
-					<li>
-						<Link to="/dashboard">
-							<i className="fa fa-tachometer"></i> Dashboard
-						</Link>
-					</li>
-
-					<li>
-						<a
-							href="#productSubmenu"
-							data-toggle="collapse"
-							aria-expanded="false"
-							className="dropdown-toggle"
-						>
-							<i className="fa fa-product-hunt"></i> Products
-						</a>
-						<ul className="collapse list-unstyled" id="productSubmenu">
-							<li>
-								<Link to="/admin/products">
-									<i className="fa fa-clipboard"></i> All
-								</Link>
-							</li>
-
-							<li>
-								<Link to="/admin/product">
-									<i className="fa fa-plus"></i> Create
-								</Link>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<a
-							href="#categorySubmenu"
-							data-toggle="collapse"
-							aria-expanded="false"
-							className="dropdown-toggle"
-						>
-							<i className="fa fa-product-hunt"></i> Category
-						</a>
-						<ul className="collapse list-unstyled" id="categorySubmenu">
-							<li>
-								<Link to="/admin/category">
-									<i className="fa fa-clipboard"></i> All
-								</Link>
-							</li>
-
-							<li>
-								<Link to="/admin/category/new">
-									<i className="fa fa-plus"></i> Create
-								</Link>
-							</li>
-						</ul>
-					</li>
-
-					<li>
-						<Link to="/admin/orders">
-							<i className="fa fa-shopping-basket"></i> Orders
-						</Link>
-					</li>
-
-					<li>
-						<Link to="/admin/users">
-							<i className="fa fa-users"></i> Users
-						</Link>
-					</li>
-
-					<li>
-						<Link to="/admin/reviews">
-							<i className="fa fa-star"></i> Reviews
-						</Link>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	);
+const Sidebar = ({ sidebarOpen, toggleSidebar }) => {
+  return (
+    <div className={`${sidebarOpen ? 'w-20' : 'w-64'}`}>
+      <nav id="sidebar" className={`bg-red-800 ${sidebarOpen ? 'w-20' : 'w-32'}`}>
+        <div className="toggle-btn" onClick={toggleSidebar}>
+          <FaBars className="text-white cursor-pointer" />
+        </div>
+        <ul className="list-unstyled components mt-4">
+          <li>
+            <Link to="/dashboard" className="text-white">
+              <FaTachometerAlt /> Dashboard
+            </Link>
+          </li>
+          <li>
+            <a
+              href="#productSubmenu"
+              data-toggle="collapse"
+              aria-expanded="false"
+              className={`dropdown-toggle text-white ${sidebarOpen ? 'show' : ''}`}
+            >
+              <FaProductHunt /> Products
+            </a>
+            <ul
+              className={`collapse list-unstyled ${sidebarOpen ? 'show' : ''}`}
+              id="productSubmenu"
+            >
+              <li>
+                <Link to="/admin/products" className="text-white">
+                  <FaClipboard /> All
+                </Link>
+              </li>
+              <li>
+                <Link to="/admin/product" className="text-white">
+                  <FaPlus /> Create
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a
+              href="#categorySubmenu"
+              data-toggle="collapse"
+              aria-expanded="false"
+              className={`dropdown-toggle text-white ${sidebarOpen ? 'show' : ''}`}
+            >
+              <FaProductHunt /> Category
+            </a>
+            <ul
+              className={`collapse list-unstyled ${sidebarOpen ? 'show' : ''}`}
+              id="categorySubmenu"
+            >
+              <li>
+                <Link to="/admin/category" className="text-white">
+                  <FaClipboard /> All
+                </Link>
+              </li>
+              <li>
+                <Link to="/admin/category/new" className="text-white">
+                  <FaPlus /> Create
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link to="/admin/orders" className="text-white">
+              <FaShoppingBasket /> Orders
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin/users" className="text-white">
+              <FaUsers /> Users
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin/reviews" className="text-white">
+              <FaStar /> Reviews
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Sidebar;
