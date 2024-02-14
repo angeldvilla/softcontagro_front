9c6f98db8b05b23a059aad2090c244768fb6291a
Angel David Villa Luján <89151224+angeldvilla@users.noreply.github.com>
2024-02-14 17:21:13 -0500

    Update userReducers.js

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -92,8 +92,7 @@ export const authReducer = (state = { user: {}, isAuthenticated: false }, action
 
     case LOGOUT_SUCCESS:
       return {
-        ...state,
-        isAuthenticated: false,
+        isAuthenticated: null,
         user: null,
         stripeKey: null, 
       };
