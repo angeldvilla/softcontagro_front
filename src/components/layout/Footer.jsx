7eead233bf3aa713f30619fd064813bbe75b14d4
angeldvilla <escarlata.2003@gmail.com>
2024-08-10 12:51:41 -0500

    Refactor footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -17,26 +17,26 @@ const Footer = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div className="mb-8">
               <div className="footer-title">
-                <h3 className="text-2xl font-bold mb-4 hover:text-3xl transition-all duration-300 font-sans">
+                <h3 className="text-2xl font-bold mb-4 font-sans">
                   SoftContAgro
                 </h3>
               </div>
               <div className="footerabout-content">
-                <p className="text-xl hover:text-2xl transition-all duration-300">
+                <p className="text-xl">
                   Primera tienda virtual en La Unión Valle desde 2021.
                 </p>
               </div>
               <div className="footer-address mt-8">
                 <ul>
-                  <p className="flex items-center text-lg hover:text-2xl transition-all duration-300">
+                  <p className="flex items-center text-lg">
                     <FaPhone className="mr-2" />
                     +57 312558468
                   </p>
-                  <p className="flex items-center text-lg hover:text-2xl transition-all duration-300">
+                  <p className="flex items-center text-lg">
                     <FaEnvelope className="mr-2" />
                     fincalalolita@gmail.com
                   </p>
-                  <p className="flex items-center text-lg hover:text-2xl transition-all duration-300">
+                  <p className="flex items-center text-lg">
                     <FaLocationDot className="mr-2" />
                     Colombia - La Union, Valle
                   </p>
@@ -49,7 +49,7 @@ const Footer = () => {
                   <img
                     src="https://res.cloudinary.com/dxe4igvmq/image/upload/v1702051130/SoftContAgro/eb2emrt90t6vpse9dapa.png"
                     alt="logo"
-                    className="w-80 hover:w-96 transition-all duration-300"
+                    className="w-80"
                   />
                 </a>
               </div>
@@ -57,27 +57,27 @@ const Footer = () => {
             </div>
             <div className="mb-8">
               <div className="footer-title">
-                <h3 className="text-2xl font-bold hover:text-3xl transition-all duration-300 font-sans">
+                <h3 className="text-2xl font-bold font-sans">
                   Redes Sociales
                 </h3>
               </div>
               <div className="footer-link mt-4">
                 <ul>
-                  <p className="flex items-center text-xl hover:text-2xl transition-all duration-300">
+                  <p className="flex items-center text-xl">
                     <FaFacebook className="mr-2" />
                     Facebook
                   </p>
-                  <p className="flex items-center text-xl hover:text-2xl transition-all duration-300">
+                  <p className="flex items-center text-xl">
                     <FaInstagram className="mr-2" /> Instagram
                   </p>
-                  <p className="flex items-center text-xl hover:text-2xl transition-all duration-300">
+                  <p className="flex items-center text-xl">
                     <FaTwitter className="mr-2" /> Twitter
                   </p>
-                  <p className="flex items-center text-xl hover:text-2xl transition-all duration-300">
+                  <p className="flex items-center text-xl">
                     <FaYoutube className="mr-2" />
                     Youtube
                   </p>
-                  <p className="flex items-center text-xl hover:text-2xl transition-all duration-300"></p>
+                  <p className="flex items-center text-xl"></p>
                 </ul>
               </div>
             </div>
@@ -85,7 +85,7 @@ const Footer = () => {
           <div className="flex justify-between items-center mt-8">
             <div className="text-center md:text-left">
               <div className="copy-right-area">
-                <p className="copy-text hover:text-lg transition-all duration-300">
+                <p className="copy-text">
                   Copyright © {new Date().getFullYear()} Finca La Lolita.
                   <a href="/">Todos los derechos reservados</a>
                 </p>
